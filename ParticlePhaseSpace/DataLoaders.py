017efa86a92a78a3b661e38534a175c60e58fcec
bwhe3635 <brendan.whelan@sydney.edu.au>
2023-01-20 21:29:09 +1100

    got export test working, may have broken some others

--- ParticlePhaseSpace/DataLoaders.py
+++ ParticlePhaseSpace/DataLoaders.py
@@ -68,6 +68,10 @@ class _DataLoadersBase(ABC):
         if self.data.isnull().values.any():
             raise AttributeError(f'input data may not contain NaNs')
 
+        tot_mom = np.sqrt(self.data['px [MeV/c]']**2 + self.data['py [MeV/c]']**2 + self.data['pz [MeV/c]']**2)
+        if not np.min(tot_mom)>0:
+            raise Exception('particles with zero absolute momentum make no sense')
+
         # is every particle ID unique?
         if not len(self.data['particle id'].unique()) == len(self.data['particle id']):
             raise Exception('you have attempted to create a data set with non'
@@ -128,7 +132,7 @@ class LoadTopasData(_DataLoadersBase):
         self.data['px [MeV/c]'] = np.multiply(P, DirCosineX)
         self.data['py [MeV/c]'] = np.multiply(P, DirCosineY)
         temp = P ** 2 - self.data['px [MeV/c]'] ** 2 - self.data['py [MeV/c]'] ** 2
-        ParticleDir = [1 if elem else -1 for elem in ParticleDir]
+        ParticleDir = [-1 if elem else 1 for elem in ParticleDir]
         self.data['pz [MeV/c]'] = np.multiply(np.sqrt(temp), ParticleDir)
         self._check_energy_consistency(Ek=E)
 
@@ -177,5 +181,8 @@ class LoadPandasData(_DataLoadersBase):
         is pandas instance
         """
         assert isinstance(self._input_data, pd.DataFrame)
+
+        if self._particle_type:
+            raise AttributeError('particle_type should not be specified for pandas import')
 
 
--- tests/test_export.py
+++ tests/test_export.py
@@ -8,6 +8,7 @@ sys.path.insert(0, str(this_file_loc.parent))
 from ParticlePhaseSpace import DataExporters
 from ParticlePhaseSpace import DataLoaders
 from ParticlePhaseSpace import PhaseSpace
+import pytest
 
 def test_topas_export():
 
@@ -15,21 +16,22 @@ def test_topas_export():
                     {'x [mm]': [0, 1, 2],
                      'y [mm]': [0, 1, 2],
                      'z [mm]': [0, 1, 2],
-                     'px [MeV/c]': [0, 1, 2],
-                     'py [MeV/c]': [0, 1, 2],
-                     'pz [MeV/c]': [0, 1, 2],
+                     'px [MeV/c]': [1, 1, 2],
+                     'py [MeV/c]': [1, 1, 2],
+                     'pz [MeV/c]': [1, 1, 2],
                      'particle type [pdg_code]': [11, 11, 11],
                      'weight': [0, 1, 2],
                      'particle id': [0, 1, 2],
-                     'time [ps]': [0, 1, 2]})
+                     'time [ps]': [0, 0, 0]})
 
-    data = DataLoaders.LoadPandasData(demo_data, particle_type='electrons')
+    data = DataLoaders.LoadPandasData(demo_data)
     PS = PhaseSpace(data)
     # ok: can we export this data:
     DataExporters.Topas_Exporter(PS,output_location='.', output_name='test.phsp')
     # now check we can read it back in:
-    # data = DataLoaders.LoadTopasData('test.phsp')
-    # PS2 = PhaseSpace(data)
-    # assert np.allclose(PS.ps_data, PS2.ps_data)
-
+    data = DataLoaders.LoadTopasData('test.phsp')
+    PS2 = PhaseSpace(data)
+    PS.reset_phase_space()
+    gah = PS.ps_data - PS2.ps_data
+    assert all(gah.max() < 1e-5)
 
