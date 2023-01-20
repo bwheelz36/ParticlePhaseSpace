2d8e331e497697912fd3097f3d98582066db1bd5
brendan <brendan.whelan@sydney.edu.au>
2023-01-20 20:08:55 +1100

    updating tests, switching computers

--- tests/test_ParticlePhaseSpace.py
+++ tests/test_ParticlePhaseSpace.py
@@ -8,6 +8,7 @@ sys.path.insert(0, str(this_file_loc.parent))
 from ParticlePhaseSpace import DataLoaders
 from ParticlePhaseSpace import PhaseSpace
 import ParticlePhaseSpace.__phase_space_config__ as ps_cfg
+import pytest
 import ParticlePhaseSpace.__particle_config__ as particle_cfg
 
 test_data_loc = this_file_loc / 'test_data'
@@ -19,25 +20,24 @@ def test_all_allowed_columns_can_be_filled():
 
     for col in ps_cfg.allowed_columns:
         if col == 'Ek [MeV]':
-            if col == 'Ek [MeV]':
-                PS.fill_kinetic_E()
-            elif col == 'rest mass [MeV/c^2]':
-                PS.fill_rest_mass()
-            elif col in 'gamma, beta':
-                PS.fill_beta_and_gamma()
-            elif col in 'vx [m/s], vy [m/s], vz [m/s]':
-                PS.fill_velocity()
-            elif col in 'Direction Cosine X, Direction Cosine Y, Direction Cosine Z':
-                PS.fill_direction_cosines()
-            else:
-                raise Exception(f'unable to fill required column {col}')
+            PS.fill_kinetic_E()
+        elif col == 'rest mass [MeV/c^2]':
+            PS.fill_rest_mass()
+        elif col in 'gamma, beta':
+            PS.fill_beta_and_gamma()
+        elif col in 'vx [m/s], vy [m/s], vz [m/s]':
+            PS.fill_velocity()
+        elif col in 'Direction Cosine X, Direction Cosine Y, Direction Cosine Z':
+            PS.fill_direction_cosines()
+        else:
+            raise Exception(f'unable to fill required column {col}')
 
 def test_downsample_phase_space():
 
     PS_downsample = PS.get_downsampled_phase_space(downsample_factor=10)
     assert len(PS)/len(PS_downsample) > 9.5 and len(PS)/len(PS_downsample) < 10.5  # to account for rounding
 
-def test_twiss():
+def test_twiss_stability():
 
     PS.calculate_twiss_parameters(beam_direction='z')
     # compare with previous calc
@@ -49,6 +49,10 @@ def test_twiss():
             for twiss_key  in old_data[particle_key][direction_key]:
                 np.allclose(old_data[particle_key][direction_key][twiss_key],
                             PS.twiss_parameters[particle_key][direction_key][twiss_key])
+
+def test_energy_stats_stability():
+
+    PS.calculate_energy_statistics()
 
 def test_project_particles():
     if not 'vx [m/s]' in PS.ps_data.columns:
@@ -82,3 +86,34 @@ def test_get_particle_density():
     particle_density = PS.assess_density_versus_r()
     old_density = pd.read_csv(test_data_loc / 'particle_density.csv', index_col=0).squeeze("columns")
     np.allclose(particle_density, old_density)
+
+def test_get_seperated_phase_space():
+
+    # had to reload the data for some reason:
+    data = DataLoaders.LoadTopasData(test_data_loc / 'coll_PhaseSpace_xAng_0.00_yAng_0.00_angular_error_0.0.phsp')
+    PS = PhaseSpace(data)
+
+    electron_PS = PS('electrons')
+    electron_PS2= PS(11)
+    assert np.allclose(electron_PS.ps_data, electron_PS2.ps_data)
+    assert len(electron_PS) == 2853
+
+    no_electron_PS = PS - electron_PS
+
+def test_add_phase_space():
+    # had to reload the data for some reason:
+    data = DataLoaders.LoadTopasData(test_data_loc / 'coll_PhaseSpace_xAng_0.00_yAng_0.00_angular_error_0.0.phsp')
+    PS = PhaseSpace(data)
+
+    gamma_PS, electron_PS = PS(['gammas', 'electrons'])
+
+    combine_PS = gamma_PS + electron_PS
+    assert len(combine_PS) == len(gamma_PS) + len(electron_PS)
+    with pytest.raises(Exception):
+        wont_work_PS = gamma_PS + gamma_PS
+
+def test_manipulate_data():
+
+    old_x_mean = PS.ps_data['x [mm]'].mean()
+    PS.ps_data['x [mm]'] = PS.ps_data['x [mm]'] + 2
+    assert np.allclose(PS.ps_data['x [mm]'].mean(), old_x_mean + 2)
