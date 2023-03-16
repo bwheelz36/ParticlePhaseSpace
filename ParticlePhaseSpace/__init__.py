e53ffba6cc26c45b7f57bec63157ec1b17e47de8
bwheelz36 <64300337+bwheelz36@users.noreply.github.com>
2023-03-17 10:03:36 +1100

    Update __init__.py

--- ParticlePhaseSpace/__init__.py
+++ ParticlePhaseSpace/__init__.py
@@ -1,7 +1,7 @@
 """
 This library allows a user to plot a sine wave. It's fully sick.
 """
-__version__ = '0.4.1'
+__version__ = '0.5.0'
 
 # from ParticlePhaseSpace._ParticlePhaseSpace import PhaseSpace
 from ParticlePhaseSpace.__unit_config__ import ParticlePhaseSpaceUnits, UnitSet
