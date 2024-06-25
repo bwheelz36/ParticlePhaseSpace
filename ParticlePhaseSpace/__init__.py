c952a8c12d9cbcb7125a6da7d187b9b03cd126e2
bwheelz36 <64300337+bwheelz36@users.noreply.github.com>
2024-06-25 13:57:07 +1000

    Update __init__.py

--- ParticlePhaseSpace/__init__.py
+++ ParticlePhaseSpace/__init__.py
@@ -1,5 +1,5 @@
 import importlib.metadata
-__version__ = importlib.metadata.version('bayesian-optimization')
+__version__ = importlib.metadata.version('ParticlePhaseSpace')
 
 # from ParticlePhaseSpace._ParticlePhaseSpace import PhaseSpace
 from ParticlePhaseSpace.__unit_config__ import ParticlePhaseSpaceUnits, UnitSet
